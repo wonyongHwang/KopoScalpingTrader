60383740e23acb4d04df7c830fa0781cdba114eb
wonyongHwang <wonyong@wonyong.net>
2021-02-11 01:22:56 +0900

    update15

--- botManager.py
+++ botManager.py
@@ -7,26 +7,28 @@ import dbManager
 import pandas as pd
 import pythoncom
 import time
+import asyncio
+import threading
+import win32com.client
 
 my_token = 'your token should be here'   #토큰을 변수에 저장합니다.
-accountId = 'id'
 accountNumber = 'account number'
-accountPwd = 'account pwd'
-password = 'pwd'
-pkpwd = 'pkpwd'
+user_id = "your id"
+user_pass = "your password"
+user_certificate_pass = "pk password"
 
 
 bot = telegram.Bot(token = my_token)   #bot을 선언합니다.
-
+dbInstance = dbManager.dbManager()
 # updates = bot.getUpdates()  #업데이트 내역을 받아옵니다.
 #
 # for u in updates :   # 내역중 메세지를 출력합니다.
 #     print(u.message)
-
-#chat_id = bot.getUpdates()[-1].message.chat.id  # 가장 최근에 온 메세지의 chat id를 가져옵니다
+#
+# chat_id = bot.getUpdates()[-1].message.chat.id  # 가장 최근에 온 메세지의 chat id를 가져옵니다
 # print(chat_id)
 # bot.sendMessage(chat_id=chat_id, text="저는 봇입니다.")
-
+#
 # res = bot.sendMessage(chat_id='-1001490131647', text="I'm bot") #@channel로 메세지를 보냅니다.
 # print(res)
 
@@ -44,12 +46,24 @@ print('start telegram chat bot')
 def get_message(update, context) :
     # update.message.reply_text("<got text>")
     # update.message.reply_text(update.message.text)
+    print(update.channel_post.text)
+    temp = bot.getUpdates()
+    for u in temp:
+        print(u.message)
+    res = bot.sendMessage(chat_id='-1001490131647', text=update.channel_post.text+" >> I'm bot") #@channel로 메세지를 보냅니다.
+    print(res)
     return
 
-def queryProfit(tempDate='', isToday = True):
-    # pythoncom.CoInitialize()
-    # orderManager.Login(id=accountId, pwd=password, cert=pkpwd)
+def worker(tempDate,isToday, loop):
+    asyncio.set_event_loop(loop)
+    loop.run_until_complete(queryProfit(tempDate,isToday))
 
+async def queryProfit(tempDate='', isToday = True):
+
+    pythoncom.CoInitialize()
+    orderManager.Login(id=user_id, pwd=user_pass, cert=user_certificate_pass)
+
+    print("queryProfit called")
     dbInstance = dbManager.dbManager()
     res = dbInstance.selectOrderListByDate(tempDate)
     if len(res) == 0:
@@ -64,6 +78,7 @@ def queryProfit(tempDate='', isToday = True):
     if df.shape[0] == 0:
         print("매매내역 조회 실패")
         sendTeleMsg("매매내역 조회 실패")
+        pythoncom.CoUninitialize()
         return
     totmdsum = 0
     totmssum = 0
@@ -72,6 +87,7 @@ def queryProfit(tempDate='', isToday = True):
         tmpdf = df[(df['종목번호'] == i["shcode"]) & (df['매매구분'] == '매도')]
         if tmpdf.shape[0] == 0:
             print("해당 종목 해당일 매도 내역 없음")
+            pythoncom.CoUninitialize()
             continue
         mdsum = tmpdf['정산금액'].apply(pd.to_numeric).sum()
         totmdsum += mdsum
@@ -95,7 +111,7 @@ def queryProfit(tempDate='', isToday = True):
         if mssum == 0:
             print("매수금액 계산 오류")
             continue
-        profitRate = (((float(mdsum) * 0.9972 - float(mssum))) / float(mssum))
+        profitRate = (((float(mdsum) * 0.9974 - float(mssum))) / float(mssum))
         profitRate = format(profitRate, "3.2%")  # '{:.3%}".format(0.25666)
         tmpStr = i['isunm'] + ' 매수금액: ' + str(mssum) + ' 매도금액: ' + str(mdsum) + ' 수익률: ' + str(profitRate) + '\n'
         print(tmpStr, end='')
@@ -105,12 +121,13 @@ def queryProfit(tempDate='', isToday = True):
 
     if totmssum == 0:
         print("정산 대상 총매수금액 : 0")
+        pythoncom.CoUninitialize()
         return
     tmpStr = '총매수: ' + str(totmssum) + ' 총매도: ' + str(totmdsum) + ' 총수익: ' + format(
-        (float(totmdsum) * 0.9972 - float(totmssum)) / float(totmssum), "10.2%")
+        (float(totmdsum) * 0.9974 - float(totmssum)) / float(totmssum), "10.2%")
     print(tmpStr)
     sendTeleMsg(tmpStr)
-    # pythoncom.CoUninitialize()
+    pythoncom.CoUninitialize()
 
 
 # help reply function
@@ -123,25 +140,37 @@ def queryMine(isToday=True,date='') :
     else :
         queryProfit(date,~isToday)
 
-# def help_command(update, context) :
-#         update.message.reply_text("계좌 조회..")
-#         if len(context.args) == 0:
-#             now = time.localtime()
-#             tempDate = "%04d%02d%02d" % (now.tm_year, now.tm_mon, now.tm_mday - 1)
-#             queryProfit(tempDate, True)
-#         else:
-#             print("args[0]", context.args[0])
-#             queryProfit(context.args[0], False)
+def help_command(update, context) :
+    # pythoncom.CoInitialize()
+    # orderManager.Login(id=accountId, pwd=password, cert=pkpwd)
 
-# updater = Updater(my_token, use_context=True)
-#
-# message_handler = MessageHandler(Filters.text & (~Filters.command), get_message) # 메세지중에서 command 제외
-# updater.dispatcher.add_handler(message_handler)
-#
-# help_handler = CommandHandler('st', help_command)
-# updater.dispatcher.add_handler(help_handler)
-#
-#
-# updater.start_polling(timeout=3, clean=True)
-# updater.idle()
+    update.message.reply_text("계좌 조회..")
+    if len(context.args) == 0:
+        update.message.reply_text("...")
+        now = time.localtime()
+        tempDate = "%04d%02d%02d" % (now.tm_year, now.tm_mon, now.tm_mday )
+        # loop = asyncio.get_event_loop()
+        # loop.run_until_complete(queryProfit(tempDate, True))
+        # loop.close()
+        loop = asyncio.new_event_loop()
+        p = threading.Thread(target=worker, args=(tempDate, True, loop,))
+        p.start()
+        p.join()
+        # queryProfit(tempDate, True)
+    else:
+        print("args[0]", context.args[0])
+        queryProfit(context.args[0], False)
+    # pythoncom.CoUninitialize()
+
+updater = Updater(my_token, use_context=True)
+
+message_handler = MessageHandler(Filters.text & (~Filters.command), get_message) # 메세지중에서 command 제외
+updater.dispatcher.add_handler(message_handler)
+
+help_handler = CommandHandler('st', help_command)
+updater.dispatcher.add_handler(help_handler)
+
+
+updater.start_polling(timeout=3, clean=True)
+updater.idle()
 
