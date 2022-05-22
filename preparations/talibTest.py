28326a4b15c06b602ec79ae12c7eef310dce98b9
wayne hwang <wonyong@wonyong.net>
2022-05-22 16:33:03 +0900

    talib cci stochastic added

--- preparations/talibTest.py
+++ preparations/talibTest.py
@@ -30,9 +30,7 @@ macd, macdsignal, macdhist = ta.MACD(df['종가'], fastperiod=12, slowperiod=26,
 macd = macd[~np.isnan(macd)]  # remove nan
 macdsignal = macdsignal[~np.isnan(macdsignal)]  # remove nan
 macdhist = macdhist[~np.isnan(macdhist)]  # remove nan
-print(macd)
-print(macdsignal)
-print(macdhist)
+print("macd>> ",macd)
 
 upper, middle, low = ta.BBANDS(df['종가'], 20, 3, 3)  # 3 sigma
 upper = np.asarray(upper)
@@ -42,44 +40,80 @@ low = np.array(low)
 obv = ta.OBV(np.asarray(df['종가'], dtype=np.double), np.asarray(df['거래량'], dtype=np.double))
 obvSignal = ta.MA(obv, timeperiod=9)
 
+fastk, fastd = ta.STOCHF(high=df['고가'], low=df['저가'], close=df['종가'], fastk_period=12, fastd_period=3, fastd_matype=0) # SMA
+fastk = fastk[~np.isnan(fastk)]
+fastd = fastd[~np.isnan(fastd)]
+print("fastk>> ", fastk)
+print("fastd>> ", fastd)
+
+slowk, slowd = ta.STOCH(high=df['고가'], low=df['저가'], close=df['종가'], fastk_period=12, slowk_period=3, slowk_matype=0,slowd_period=3,slowd_matype=0)
+slowk = slowk[~np.isnan(slowk)]
+slowd = slowd[~np.isnan(slowd)]
+print("slowk>> ", fastk)
+print("slowd>> ", fastd)
+
+cci = ta.CCI(high=df['고가'], low=df['저가'], close=df['종가'],timeperiod=14)
+cciSig = ta.MA(cci, timeperiod=9)
+cci = cci[~np.isnan(cci)]
+cciSig = cciSig[~np.isnan(cciSig)]
+print("cci>> ",cci)
+
 plt.figure(figsize=(30, 30))
-plt.subplot(231)
+plt.subplot(331)
 plt.title(str(shcode))
 plt.plot(obv)
 plt.plot(obvSignal)
 plt.legend(["obv", "obv signal"])
 
-plt.subplot(232)
+plt.subplot(332)
 plt.plot(rsi9)
 plt.plot(simpleMA9)
 # plt.text(0, 10, "rg:" + str(format(grad, "3.2%")))
 plt.legend(["rsi", "rsi signal"])
 
-plt.subplot(233)
+plt.subplot(333)
 plt.plot(taadx)
 plt.plot(taadxSig)
 plt.legend(["adx", "adx sig"])
 
-ax = plt.subplot(234)
+ax = plt.subplot(334)
 dfnew = df[['시가', '고가', '저가', '종가']]
 day_list = range(len(df))
 dfnew.insert(0, '시각', day_list)
 dfnew = dfnew.apply(pd.to_numeric)
 candlestick_ohlc(ax, dfnew.values, width=0.5, colorup='r', colordown='b')
 
-plt.subplot(235)
+plt.subplot(335)
 plt.title("MACD")
 plt.plot(macd)
 plt.plot(macdsignal)
 plt.plot(macdhist)
 plt.legend(["macd", "macdsignal","macdhist"])
 
-plt.subplot(236)
+plt.subplot(336)
 plt.title("Bollinger Band")
 plt.plot(upper)
 plt.plot(middle)
 plt.plot(low)
 plt.legend(["upper", "middle","low"])
+
+plt.subplot(337)
+plt.title("Stochastic Fast  K={} D={}".format(12,3))
+plt.plot(fastk)
+plt.plot(fastd)
+plt.legend(["fastk", "fastd"])
+
+plt.subplot(338)
+plt.title("Stochastic Slow K=%d D=%d" % (12,3))
+plt.plot(slowk)
+plt.plot(slowd)
+plt.legend(["slowk", "slowd"])
+
+plt.subplot(339)
+plt.title("CCI")
+plt.plot(cci)
+plt.plot(cciSig)
+plt.legend(["cci", "cciSig"])
 
 plt.grid()
 plt.show()
